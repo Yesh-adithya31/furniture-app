796a7ea0caa44163a4a639db0c03d9e8e002edb4
Yesh-adithya31 <57474408+Yesh-adithya31@users.noreply.github.com>
2023-09-02 01:03:36 +0530

    Add: Initialized imported fonts in App.js(main File)

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require('./assets/fonts/OpenSans-Regular.ttf'),
+    light: require('./assets/fonts/OpenSans-Light.ttf'),
+    bold: require('./assets/fonts/OpenSans-Bold.ttf'),
+    medium: require('./assets/fonts/OpenSans-Medium.ttf'),
+    extrabold: require('./assets/fonts/OpenSans-ExtraBold.ttf'),
+    senibold: require('./assets/fonts/OpenSans-SemiBold.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded){
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyles}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyles:{
+    fontFamily: "extrabold",
+    fontSize: 29
+  }
 });
